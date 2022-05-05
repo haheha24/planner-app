fb261d66f046e1244254eedd8f8097f7d8691066
haheha24 <haheha24@users.noreply.github.com>
2022-05-05 21:08:54 +1000

    refactored response chains.

--- pages/api/theme/setTheme.ts
+++ pages/api/theme/setTheme.ts
@@ -2,24 +2,28 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import cookie from "cookie";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    res.setHeader(
-      "Set-Cookie",
-      cookie.serialize("themeMode", JSON.stringify(req.body.userTheme), {
-        /* httpOnly: true, */
-        secure: process.env.NODE_ENV !== "development",
-        sameSite: "strict",
-        path: "/",
-      })
-    );
-    res.status(200);
-    res.json({ success: true });
-    return
-  } catch {
-    res.status(500);
-    res.json({
-      success: false,
-      error: `unable to set cookie ${req.body.theme}`,
-    });
+  if (req.method === "POST") {
+    try {
+      res
+        .setHeader(
+          "Set-Cookie",
+          cookie.serialize("themeMode", JSON.stringify(req.body.userTheme), {
+            /* httpOnly: true, */
+            secure: process.env.NODE_ENV !== "development",
+            sameSite: "strict",
+            path: "/",
+          })
+        )
+        .status(200)
+        .json({ success: true });
+      return;
+    } catch {
+      res.status(500).json({
+        success: false,
+        error: `unable to set cookie ${req.body.theme}`,
+      });
+    }
+  } else {
+    res.status(400).json({ success: false, error: `Request does not exist.` });
   }
 };
