55f741acd6c1ad091e47f48bf33be283a0a94d29
haheha24 <haheha24@users.noreply.github.com>
2022-05-26 12:15:52 +1000

    Added more placeholder css to make the footer more populated with styled-components

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,5 +1,16 @@
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+  background-color: ${({ theme }) => theme.body};
+  color: ${({ theme }) => theme.text};
+`;
+
 const Footer = () => {
-  return <footer><div>Copyright © Adrian Cristallo</div></footer>;
+  return (
+    <StyledFooter>
+      <div>Copyright © Adrian Cristallo</div>
+    </StyledFooter>
+  );
 };
 
 export default Footer;
