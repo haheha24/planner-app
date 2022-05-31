a734d49823f82b11cd2e1dc7233bc8615edda90d
haheha24 <haheha24@users.noreply.github.com>
2022-05-31 18:04:40 +1000

    added sessionprovider for NextAuth to share the session state through context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 import { StyledThemeProvider } from "../hooks/ThemeContext";
-import Layout from "../components/layout/Layout";
 
 //Mocks
 if (process.env.NEXT_PUBLIC_API_MOCKING === "true") {
@@ -10,12 +10,12 @@ if (process.env.NEXT_PUBLIC_API_MOCKING === "true") {
   });
 }
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <StyledThemeProvider>
-      <Layout>
+      <SessionProvider session={session}>
         <Component {...pageProps} />
-      </Layout>
+      </SessionProvider>
     </StyledThemeProvider>
   );
 }
